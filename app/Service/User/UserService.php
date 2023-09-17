a130d7bba21e1a294377c7b42139bececb6eafb8
Demetrius Lewis <DemetriusLewis2012@gmail.com>
2023-09-17 17:06:54 -0400

    initial commit

---
+++ app/Service/User/UserService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Service\User;
+
+use App\Models\User;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+
+class UserService
+{
+    public function __construct(private User $userModel)
+    {
+    }
+
+    public function register(array $data)
+    {
+        $user = $this->userModel->create($data);
+
+        return array_merge($user, $this->respondWithToken($user));
+    }
+
+    public function respondWithToken($user = null)
+    {
+        $token = JWTAuth::fromUser($user ?? auth()->user());
+
+        return [
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+        ];
+    }
+}
