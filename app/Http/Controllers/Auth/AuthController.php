54b1274eb6ba6f8c1cb709d84cb32d1aa011a28c
Demetrius Lewis <DemetriusLewis2012@gmail.com>
2023-09-19 18:01:48 -0400

    feat: user login feature

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Exceptions\CustomException;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterUserRequest;
+use App\Http\Requests\Auth\LoginUserRequest;
 use App\Service\User\UserService;
 use Exception;
 
@@ -20,7 +22,21 @@ class AuthController extends Controller
             return response()->success('user created', $user, 201);
         } catch(Exception $e) {
             report($e);
-            return response()->errorResponse('something went wrong', $e);
+            return response()->errorResponse('something went wrong');
+        }
+    }
+
+    public function login(LoginUserRequest $request)
+    {
+        try{
+            $user = $this->userService->authenticate($request->only(['email', 'password']));
+            return response()->success('login successful', $user);
+        }catch (CustomException $e) {
+            report($e);
+            return response()->errorResponse($e->getMessage(), [], $e->getCode());
+        } catch(Exception $e) {
+            report($e);
+            return response()->errorResponse($e->getMessage(), [], 500);
         }
     }
 }
--- app/Service/User/UserService.php
+++ app/Service/User/UserService.php
@@ -2,6 +2,8 @@
 
 namespace App\Service\User;
 
+use App\Exceptions\CustomException;
+use App\Http\Resources\User\UserResource;
 use App\Models\User;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
@@ -15,17 +17,43 @@ class UserService
     {
         $user = $this->userModel->create($data);
 
-        return array_merge($user->toArray(), $this->respondWithToken($user));
+        return array_merge($user->toArray(), $this->respondWithToken($user)->toArray());
     }
 
     public function respondWithToken($user = null)
     {
         $token = JWTAuth::fromUser($user ?? auth()->user());
 
-        return [
+        return (object) [
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth('api')->factory()->getTTL() * 60,
         ];
+    }
+
+    public function authenticate(array $request)
+    {
+        $email = $request['email'];
+        $user = $this->userModel->firstWhere('email', $email);
+
+        if (!$user) {
+            throw new CustomException('Invalid Credentials', 401);
+        }
+
+        if (!auth('web')->attempt(['email' => $email, 'password' => $request['password']])){
+            throw new CustomException('Invalid Credentials', 401);
+        }
+
+        $token = $this->respondWithToken($user);
+
+        return $this->append_token_to_user($token, $user);
+    }
+
+    public function append_token_to_user($token, User $user)
+    {
+        if(!is_object($token) || !property_exists($token, 'access_token')) {
+            throw new CustomException('Invalid data', 400);
+        }
+        return array_merge((new UserResource($user))->toArray(request()), ['token' => $token]);
     }
 }
